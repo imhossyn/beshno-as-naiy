84b72c6a51cc56d74ea11bbbe4a8c3582746b5fa
mrmysm <mmysmmh@gamil.com>
2024-04-28 20:14:42 +0330

    fix bug splash activity

--- app/src/main/java/com/fateh/beshno_as_naiy/SplashActivity.java
+++ app/src/main/java/com/fateh/beshno_as_naiy/SplashActivity.java
@@ -1,30 +1,27 @@
 package com.fateh.beshno_as_naiy;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
-
-    private static final int SPLASH_DURATION = 3000; // 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Start the main activity after the splash screen duration
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
                 finish();
             }
-        }, SPLASH_DURATION);
+        }, 2000);
     }
 }
